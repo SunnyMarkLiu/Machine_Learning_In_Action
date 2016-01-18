f34da402b8b14202ad8c49ac9d054a6f49fc2338
SunnyLQ <SunnyMarkLiu101@gmail.com>
2016-01-18 16:25:49 +0800

    add the plot Sigmoid function

---
+++ LogisticRegression/plotSigmoid.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7
+# _*_ coding: utf-8 _*_
+
+"""
+@author: MarkLiu
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plotSigmoidTest():
+    """
+    绘制Sigmoid函数
+    :return:
+    """
+    figure = plt.figure(figsize=(10, 10), facecolor="white")
+    figure.clear()
+
+    pltaxes = plt.subplot(111)
+    num = np.linspace(-40, 40, 256, endpoint=True)
+
+    y = 1.0 / (1 + np.exp(-1 * num))  # 计算Sigmoid函数 y(x) = 1 / (1 + exp(-z))
+    pltaxes.plot(num, y, color="blue", linewidth=2.0, linestyle="-")
+    plt.xlim(-40, 40)
+    plt.show()
+
+
+if __name__ == '__main__':
+    plotSigmoidTest()
