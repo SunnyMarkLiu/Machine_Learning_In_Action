c3bd656b6340f07b9fc4e67961f6cbe39bca63b3
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2016-03-05 17:05:13 +0800

    Locally Weighted Regression

--- Regression/regression.py
+++ Regression/regression.py
@@ -37,13 +37,42 @@ def standardRegression(datasArr, valuessArr):
     """
     # 保证数据为numpy矩阵
     xMat = np.matrix(datasArr)
-    print "xMat:", np.shape(xMat)
     valueMat = np.matrix(valuessArr).T
-    print "valueMat:", np.shape(valueMat)
     # W = (xTx)^(-1)*xTy
     xTx = xMat.T * xMat
-    if np.linalg.det(xTx) == 0.0:   # 如果xTx矩阵的行列式为0,则不存在逆矩阵
+    if np.linalg.det(xTx) == 0.0:  # 如果xTx矩阵的行列式为0,则不存在逆矩阵
         print "xTx矩阵的行列式为0,则不存在逆矩阵!"
 
     ws = xTx.I * (xMat.T * valueMat)
     return ws
+
+
+def locallyWeightedRegression(testPoint, datasArr, valuessArr, k=1):
+    """
+    局部加权线性回归
+    :param testPoint: 测试的数据点
+    :param datasArr: 样本数据
+    :param valuessArr: 样本数据的结果
+    :param k: 控制权重下降的速度，k越小下降速度越快
+    :return:
+    """
+    # 保证数据为numpy矩阵
+    xMat = np.matrix(datasArr)
+    valueMat = np.matrix(valuessArr).T
+
+    # 为每个样本舒适化一个权重
+    m = np.shape(xMat)[0]
+    # 创建一个对角矩阵，对角线上为1
+    weights = np.matrix(np.eye(m))
+    for i in range(0, m):
+        featureDiffMat = testPoint - xMat[i]  # 将测试数据特征值和样本的差别，结果为矩阵
+        # 计算差别的平方根距离
+        distance = featureDiffMat * featureDiffMat.T
+        weights[i, i] = np.exp(distance / (-2 * k ** 2))
+    xTx = xMat.T * weights * xMat
+    if np.linalg.det(xTx) == 0.0:  # 如果xTx矩阵的行列式为0,则不存在逆矩阵
+        print "xTx矩阵的行列式为0,则不存在逆矩阵!"
+
+    ws = xTx.I * (xMat.T * weights * valueMat)
+    predictValue = testPoint * ws
+    return predictValue
--- Regression/testRegression.py
+++ Regression/testRegression.py
@@ -9,22 +9,58 @@ from regression import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-datasArr, valuessArr = loadDataSet('datasets/ex0.txt')
-# 利用标准回归计算系数W
-ws = standardRegression(datasArr, valuessArr)
 
-# 绘制原始数据
-xMat = np.matrix(datasArr)
-valueMat = np.matrix(valuessArr)
-plt.figure(figsize=(10, 10), facecolor="white")
-axes = plt.subplot(111)
-plt.scatter(xMat[:, 1].flatten().A[0], valueMat.T.flatten().A[0])
+def testStandardRegression():
+    datasArr, valuessArr = loadDataSet('datasets/ex0.txt')
+    # 利用标准回归计算系数W
+    ws = standardRegression(datasArr, valuessArr)
 
-# 绘制回归的曲线
-xCopy = xMat.copy()
-xCopy.sort(0)
-yHat = xCopy * ws
-# 绘制预测的曲线
-plt.plot(xCopy[:, 1], yHat)
+    # 绘制原始数据
+    xMat = np.matrix(datasArr)
+    valueMat = np.matrix(valuessArr)
+    plt.figure(figsize=(10, 10), facecolor="white")
+    plt.subplot(111)
+    plt.scatter(xMat[:, 1].flatten().A[0], valueMat.T.flatten().A[0])
 
-plt.show()
+    # 绘制回归的曲线
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy * ws
+    # 绘制预测的曲线
+    plt.plot(xCopy[:, 1], yHat)
+    plt.show()
+
+    # 计算预测值和实际值的相关性
+    yHat = xMat * ws
+    print np.shape(yHat), ":", np.shape(valueMat)
+    # 相关性系数
+    correlationCoefficients = np.corrcoef(yHat.T, valueMat)
+    print correlationCoefficients
+
+
+def testLocallyWeightedRegression():
+    datasArr, valuessArr = loadDataSet('datasets/ex0.txt')
+    m = np.shape(datasArr)[0]
+    predictValues = np.zeros(m)
+    for i in range(0, m):
+        predictValues[i] = \
+            locallyWeightedRegression(datasArr[i], datasArr, valuessArr, 0.01)
+
+    # 绘制原始数据
+    xMat = np.matrix(datasArr)
+    valueMat = np.matrix(valuessArr)
+    plt.figure(figsize=(10, 10), facecolor="white")
+    plt.subplot(111)
+    plt.scatter(xMat[:, 1].flatten().A[0], valueMat.T.flatten().A[0])
+    # 绘制回归的曲线
+    # 先对测试数据进行排序
+    sortedIndexs = xMat[:, 1].argsort(0)
+    sortedMat = xMat[sortedIndexs.flatten().A[0]]
+    plt.plot(sortedMat[:, 1], predictValues[sortedIndexs], c='red', linewidth=2)
+    plt.show()
+    # 计算预测值和实际值的相关性
+    correlationCoefficients = np.corrcoef(predictValues, valueMat)
+    print correlationCoefficients
+
+if __name__ == '__main__':
+    testLocallyWeightedRegression()
