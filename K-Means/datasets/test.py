e0ca8a62e0a606aa957555fd0d3107012626cf8f
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2016-03-16 14:43:50 +0800

    loadtxt

---
+++ K-Means/datasets/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+# _*_ coding: utf-8 _*_
+
+"""
+@Author: MarkLiu
+"""
+import numpy as np
+
+arr = np.loadtxt('testSet.txt', delimiter='\t')
+print arr
+print np.shape(arr)
+print type(arr[0][0])
